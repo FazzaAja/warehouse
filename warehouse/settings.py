2d33a1005f770d8cb70cd4abbb0154192f48b1f6
FazzaAJa <lanangfajri@gmail.com>
2023-02-09 22:31:36 +0700

    add gunicorn

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-r&*!%+qu%7%fk=9_e-fff89-*hex_e7iw*bio5o)fch=+=w-j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
